934d1f0569a958d62b217ca71fead357b10b0138
원치현 <heungeob1003@gmail.com>
2021-10-07 15:05:36 +0900

    Add connect MySQL server syntax

--- insert.php
+++ insert.php
@@ -1,6 +1,7 @@
 <?php
-    $mysqli = mysqli_connect("example.com", "user", "password", "database");
-    $result = mysqli_query($mysqli, "SELECT 'Please do not use the deprecated mysql extension for new development. ' AS _msg FROM DUAL");
-    $row = mysqli_fetch_assoc($result);
-    echo $row['_msg'];
+    //$mysqli = mysqli_connect("example.com", "user", "password", "database");
+    //$result = mysqli_query($mysqli, "SELECT 'Please do not use the deprecated mysql extension for new development. ' AS _msg FROM DUAL");
+    // $row = mysqli_fetch_assoc($result);
+    //echo $row['_msg'];
+    mysqli_connect("localhost","root","h20048686","opentutorials");
 ?>
