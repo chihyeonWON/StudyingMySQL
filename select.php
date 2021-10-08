f94d2ad38b2e8f736a2f5ef9d43e50e185ae2bbb
원치현 <heungeob1003@gmail.com>
2021-10-08 18:59:09 +0900

    Use var dump function to check data's number

--- select.php
+++ select.php
@@ -2,4 +2,5 @@
     $conn = mysqli_connect('127.0.0.1','root','h20048686','opentutorials');
     $sql = "SELECT * FROM topic";
     $result = mysqli_query($conn, $sql);
+    var_dump($result->num_rows);
 ?>
