02876519cb1a43f449e511a4a141a1f40117e160
원치현 <heungeob1003@gmail.com>
2021-10-08 19:30:06 +0900

    mysqli_fetch_array function execute twice

--- select.php
+++ select.php
@@ -2,10 +2,16 @@
     //database에 연결
     $conn = mysqli_connect('127.0.0.1','root','h20048686','opentutorials');
     
-    echo "<h1>single row</h1>";
-    $sql = "SELECT * FROM topic WHERE id = 5";
-    $result = mysqli_query($conn, $sql);
+    echo "<h1>multi row</h1>";
+    $sql = "SELECT * FROM topic";
+    $result = mysqli_query($conn,$sql);
+
     $row = mysqli_fetch_array($result);
-    echo "<h2>".$row['title']."</h2>";
-    echo $row['description'];
+    echo '<h2>'.$row["title"].'</h2>';
+    echo $row["description"];
+
+    $row = mysqli_fetch_array($result);
+    echo '<h2>'.$row["title"].'</h2>';
+    echo $row["description"];   
+    
 ?>
