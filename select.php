7c71f122dc0ccf091c31fdd3d5d53e044e9e6fdf
원치현 <heungeob1003@gmail.com>
2021-10-08 19:22:53 +0900

    Print specific row's data

--- select.php
+++ select.php
@@ -1,6 +1,11 @@
 <?php
+    //database에 연결
     $conn = mysqli_connect('127.0.0.1','root','h20048686','opentutorials');
-    $sql = "SELECT * FROM topic";
+    
+    echo "<h1>single row</h1>";
+    $sql = "SELECT * FROM topic WHERE id = 5";
     $result = mysqli_query($conn, $sql);
-    print_r(mysqli_fetch_array($result));
+    $row = mysqli_fetch_array($result);
+    echo "<h2>".$row['title']."</h2>";
+    echo $row['description'];
 ?>
