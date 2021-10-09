b06e621cb960fc211664656a2b3b9148cc4a73f7
원치현 <heungeob1003@gmail.com>
2021-10-09 20:11:09 +0900

    Apply list to create.php

--- create.php
+++ create.php
@@ -1,3 +1,28 @@
+<?php  
+    $conn = mysqli_connect("127.0.0.1","root","h20048686","opentutorials");
+
+    $sql = "SELECT * FROM topic";
+    $result = mysqli_query($conn,$sql);
+    $list = '';
+
+    while($row = mysqli_fetch_array($result)){
+        $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+    }
+    $article = array(
+        'title' => 'Welcome',
+        'description' => 'Hello, web'
+    );
+    //sql문을 서버에 전달하고 변수에 값 담기
+    if(isset($_GET['id'])) {
+        $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_array($result);
+        $article = array(
+            'title' => $row['title'], //배열의 인덱스가 숫자가 아닌 문자 title로 연관배열이라고 한다.
+            'description' => $row['description']
+        );
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,9 +30,9 @@
         <title></title>
     </head>
     <body>
-        <h1>WEB</h1>
+        <h1><a href="index.php">WEB</a></h1>
         <ol>
-            <li>HTML</li>
+            <?= $list ?>
         </ol>
         <form action="process_create.php" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
