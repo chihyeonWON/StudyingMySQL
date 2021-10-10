a2a01fb6f6b8003434a38436916c0886011f39b5
원치현 <heungeob1003@gmail.com>
2021-10-10 19:57:25 +0900

    Change SQL syntax update to delete

--- process_delete.php
+++ process_delete.php
@@ -6,12 +6,9 @@
         'id' => mysqli_real_escape_string($conn, $_POST['id'])
     );
     $sql = "
-        UPDATE topic
-            SET
-                title = '{$filtered['title']}',
-                description = '{$filtered['description']}'
-            WHERE
-                id = '{$filtered['id']}'
+        DELETE 
+            FROM topic
+            WHERE id = {$filtered['id']}
     ";
     $result = mysqli_query($conn, $sql);
     if($result == false) {
