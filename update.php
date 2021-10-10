3913da183107a2ae785851789e03c866ccd26259
원치현 <heungeob1003@gmail.com>
2021-10-10 19:25:42 +0900

    Delete useless code

--- update.php
+++ update.php
@@ -9,6 +9,23 @@
         $escaped_title = htmlspecialchars($row['title']);
         $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$escaped_title}</a></li>";
     }
+    $article = array(
+        'title' => 'Welcome',
+        'description' => 'Hello, web'
+    );
+
+    $update_link = '';
+
+    //sql문을 서버에 전달하고 변수에 값 담기
+    if(isset($_GET['id'])) {
+        $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
+        $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_array($result);
+        $article['title'] = htmlspecialchars($row['title']);
+        $article['descrption'] = htmlspecialchars($row['description']);
+        $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
+    }
 ?>
 <!DOCTYPE html>
 <html>
